32835271d959e4a603373a80dfe1240f085f9e76
Ben Blackman <bblkmn5@gmail.com>
2021-07-19 13:20:04 -0700

    create input, create board, create gameplay.py

---
+++ gameplay.py
@@ -0,0 +1,6 @@
+def display_board(board):
+    print("{}  |  {}  |  {} ".format(board[0], board[1], board[2]))
+    print("-------------")
+    print("{}  |  {}  |  {} ".format(board[3], board[4], board[5]))
+    print("-------------")
+    print("{}  |  {}  |  {} ".format(board[6], board[7], board[8]))
--- script.py
+++ script.py
@@ -1,10 +1,19 @@
+from gameplay import display_board
+
 #Create welcome message
 print(
-"###"+ " ###" + " ###" + "   ###"+" ###"+" ###"+"   ###"+" ###"+" ###"+"\n"
-" #" + "   #" + "  #" + "      #"+"  # #"+" #"+"      #"+"  # #"+" #"+"\n"
-" #" + "   #" + "  #" + "      #"+"  ###"+" #"+"      #"+"  # #"+" ##"+"\n"
-" #" + "   #" + "  #" + "      #"+"  # #"+" #"+"      #"+"  # #"+" #"+"\n"
-" #" + "  ###" + " ###" + "    #"+"  # #"+" ###"+"    #"+"  ###"+" ###"+ "\n"
+"TTT"+ " III" + " CCC" + "   TTT"+" AAA"+" CCC"+"   TTT"+" OOO"+" EEE"+"\n"
+" T" + "   I" + "  C" + "      T"+"  A A"+" C"+"      T"+"  O O"+" E"+"\n"
+" T" + "   I" + "  C" + "      T"+"  AAA"+" C"+"      T"+"  O O"+" EE"+"\n"
+" T" + "   I" + "  C" + "      T"+"  A A"+" C"+"      T"+"  O O"+" E"+"\n"
+" T" + "  III" + " CCC" + "    T"+"  A A"+" CCC"+"    T"+"  OOO"+" EEE"+ "\n"
 )
-print("Welcome to Tic Tac Toe! A Game you can play by yourself or with another.")
+print("Welcome to Tic Tac Toe! A game you can play by yourself or with another.")
 print("")
+players = input("How many players? (1 or 2)\n")
+print("You have selected a " + players + "-player game.\n")
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+
+display_board(board)
